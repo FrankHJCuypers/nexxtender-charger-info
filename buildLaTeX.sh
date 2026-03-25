a8e72f50fee01b25dec8c2e0f271bd71ba82d533
Frank HJ Cuypers <frank@cuypers-ghys.be>
2026-03-25 18:48:45 +0100

    build: -shell-escape is no longer needed

--- buildLaTeX.sh
+++ buildLaTeX.sh
@@ -3,4 +3,4 @@
 echo "1 $gitdescribe"
 source CreateGitData.sh
 echo "2 $gitdescribe"
-latexmk -pdf -jobname=%A$gitdescribe -shell-escape nexxtender.tex
+latexmk -pdf -jobname=%A$gitdescribe nexxtender.tex
